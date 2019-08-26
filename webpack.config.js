e4388f82c370c7b34de48850f433cea63b539c04
pavelkazancev02 <pavelkazancev02@gmail.com>
2019-08-26 22:22:43 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,30 @@ module.exports = {
       { from: './src/index.html', to: "index.html" }
     ])
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        include: /vendor/,
+        use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader',
+      }),
+      }
+    ],
+    loaders: [
+      { test: /\.json$/, use: 'json-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-runtime']
+        }
+      }
+    ]
+  },
   devServer: {
     port: process.env.PORT || 8080,
     host: '0.0.0.0',
